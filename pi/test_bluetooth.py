294b2ef7f2cffce5762844641a6593699c0d698d
Zak Dasaro <zakdasaro@gmail.com>
2021-12-04 13:10:07 -0800

    fix bug in bluetooth test

--- pi/test_bluetooth.py
+++ pi/test_bluetooth.py
@@ -8,12 +8,13 @@ kobuki_1.connect()
 
 time.sleep(4)
 positional_error = 3.65 #meters 
-heading_error = 13.23 #degrees 
+heading_error = 13.23 #degrees
+remaining_dist = 1000
 positional_error_scaled_100x = positional_error * SCALE_FACTOR #312
 heading_error_scaled_100x = heading_error * SCALE_FACTOR #1323
 print((int(positional_error_scaled_100x).to_bytes(2,'big')))
 print(int(heading_error_scaled_100x).to_bytes(2,'big'))
 print(((int(positional_error_scaled_100x).to_bytes(2,'big').hex())))
 print((int(heading_error_scaled_100x).to_bytes(2,'big').hex()))
-kobuki_1.transmit_nav(positional_error,heading_error)
+kobuki_1.transmit_nav(positional_error,heading_error,remaining_dist)
 time.sleep(10)
