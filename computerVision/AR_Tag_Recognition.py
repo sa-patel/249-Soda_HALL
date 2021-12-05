21ac696149b40d73d5cffafbf613486add3b60c3
Sagar A Patel <patel.s@berkeley.edu>
2021-12-04 22:23:13 -0800

    Add files via upload
    - Modified Distance Calibration
    
    Results: Provides distance error of less than 10% of distance at 2 meters, 3 meters and 5.25 meters

--- computerVision/AR_Tag_Recognition.py
+++ computerVision/AR_Tag_Recognition.py
@@ -1,7 +1,7 @@
 # Camera Matrix calibration, referenced OpenCV.org - Camera Calibration page
 # Imput: pre-captured images from left and right cameras
 # Output: Camera Matrix
-# Draws from OpenCV Camera Calibration and https://mecaruco2.readthedocs.io/en/latest/notebooks_rst/Aruco/aruco_basics.html
+# Draws from OpenCV Camera Calibration, https://mecaruco2.readthedocs.io/en/latest/notebooks_rst/Aruco/aruco_basics.html and https://aliyasineser.medium.com/calculation-relative-positions-of-aruco-markers-eee9cc4036e3 
 
 import numpy as np
 import cv2 as cv
@@ -52,7 +52,7 @@ new_mtxL, roiL= cv.getOptimalNewCameraMatrix(mtxL,distL,(wL,hL),1,(wL,hL))
 # Test AR_Tag recognition on AR still images. This section of code will be suplanted with live video later on
 # Show a frame with AR orientaion
 
-test_img = cv.imread("AR_Tag2.png")
+test_img = cv.imread("AR_Tag_2_meters.png")
 
 gray = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
 aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
@@ -66,10 +66,10 @@ if (ids):
 		c = corners[i][0]
 		plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "o", label = "id={0}".format(ids[i]))
 
-	size_of_marker =  0.0145 # side lenght of the marker in meter
+	size_of_marker =  0.18415 # Length of AR Tag side (in meters)
 	rvecs,tvecs, trash = aruco.estimatePoseSingleMarkers(corners, size_of_marker , new_mtxL, distL)
 
-	length_of_axis = 0.01
+	length_of_axis = 0.1
 	imaxis = aruco.drawDetectedMarkers(test_img.copy(), corners, ids)
 	for i in range(len(tvecs)):
 		imaxis = aruco.drawAxis(imaxis, new_mtxL, distL, rvecs[i], tvecs[i], length_of_axis)
