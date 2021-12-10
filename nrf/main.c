cd3f0e2f9269640ef51e5c54895898d793bdfa5e
Amit <amitrjain11@gmail.com>
2021-12-10 00:44:21 -0800

    add more state logic to nrf

--- nrf/main.c
+++ nrf/main.c
@@ -32,12 +32,21 @@
 // Project modules
 #include "motors.h"
 
- typedef enum {
-  DRIVING,
-  TURNING
+
+typedef enum {
+    IDLE, //At base station
+    RETURNING, //Going to the base station to wait for next order
+    DELIVERING_ORDER, // Bringing order to customer
+    UNLOADING, // Order is being loaded onto or unloaded from robot
+    LOADING,
+    PLAN_PATH_TO_BASE,
+    PLAN_PATH_TO_TABLE,
+    UNDEFINED
 } KobukiState_t;
 
-NRF_TWI_MNGR_DEF(twi_mngr_instance, 5, 0);
+KobukiState_t current_state = IDLE; 
+
+//NRF_TWI_MNGR_DEF(twi_mngr_instance, 5, 0);
 
 #define SCALE_FACTOR 100.00
 // Intervals for advertising and connections
@@ -57,18 +66,32 @@ static simple_ble_service_t generic_service = {{
                 0xB5,0x4D,0x22,0x2B,0x89,0x10,0xE6,0x32}
 }};
 
-static simple_ble_char_t get_button_press = {.uuid16 = 0x108e};
-static simple_ble_char_t display_string_data = {.uuid16 = 0x108d};
+// 32e61059-2b22-4db5-a914-43ce41986c70
+static simple_ble_service_t state_service= {{
+    .uuid128 = {0x70,0x6C,0x98,0x41,0xCE,0x43,0x14,0xA9,
+                0xB5,0x4D,0x22,0x2B,0x59,0x10,0xE6,0x32}
+}};
+
+
+
 static simple_ble_char_t test_error_data = {.uuid16 = 0x108c};
-static simple_ble_char_t led1_state_char = {.uuid16 = 0x108b};
+static simple_ble_char_t display_string_data = {.uuid16 = 0x108d};
+static simple_ble_char_t get_button_press = {.uuid16 = 0x108e};
+
+static simple_ble_char_t get_kobubki_state= {.uuid16 = 0x105c};
+static simple_ble_char_t led1_state_char = {.uuid16 = 0x105d};
+
+
 
 static int led_state = 1;
 static uint8_t button_press;
 static uint8_t error_data[6];
 static volatile int g_button_pressed = 0;
 
-static unsigned char buf_disp[16] = "No Drink"; 
+static unsigned char buf_disp[64]; 
+static unsigned char buf_state[16];
 KobukiSensors_t sensors = {0};
+
 //snprintf(buf, 16, "%f", measure_distance(sensors.leftWheelEncoder, previous_encoder)); 
 //display_write( buf, DISPLAY_LINE_1);
 
@@ -91,21 +114,21 @@ void ble_evt_write(ble_evt_t const* p_ble_evt) {
     if (simple_ble_is_char_event(p_ble_evt, &display_string_data)) {
       //printf("Data is : %02x %02x \n",error_data[0],error_data[1]);
       //snprintf(buf, 16, "%f", measure_distance(sensors.leftWheelEncoder, previous_encoder)); 
-      display_write((char*)buf_disp, DISPLAY_LINE_1);
+	    char delim[] = " ";
+      char *p = strtok((char*)buf_disp, delim); 
+      while (p != NULL){ 
+        display_write(p, DISPLAY_LINE_1);
+        p = strtok(NULL, delim);
+        nrf_delay_ms(2500);
+      }
     }
     if (simple_ble_is_char_event(p_ble_evt, &get_button_press)) {
       //printf("Data is : %02x %02x \n",error_data[0],error_data[1]);
       //snprintf(buf, 16, "%f", measure_distance(sensors.leftWheelEncoder, previous_encoder)); 
-      char send_buf[16];
+      //char send_buf[16];
       button_press = g_button_pressed;
       printf("button pressed %d \n",button_press);
       g_button_pressed = 0;
-
-      //KobukiSensors_t initial_sensors;
-      //kobukiSensorPoll(&initial_sensors);
-
-      //snprintf(send_buf, 16,"Drinks placed"); 
-      //display_write((char*)send_buf, DISPLAY_LINE_1);
     }
     if (simple_ble_is_char_event(p_ble_evt, &led1_state_char)) {
       printf("Got write to LED characteristic!\n");
@@ -117,6 +140,25 @@ void ble_evt_write(ble_evt_t const* p_ble_evt) {
         nrf_gpio_pin_set(BUCKLER_LED0);
       }
     }
+    if (simple_ble_is_char_event(p_ble_evt, &get_kobubki_state)) {
+      if (strcmp((char*)buf_state, "IDLE") == 0) { 
+        current_state = IDLE;
+      } else if (strcmp((char*)buf_state,"RETURNING") == 0){ 
+        current_state = RETURNING;
+      }else if (strcmp((char*)buf_state,"DELIVERING_ORDER") == 0){ 
+        current_state = DELIVERING_ORDER;
+      }else if (strcmp((char*)buf_state,"UNLOADING") == 0){ 
+        current_state = UNLOADING;
+      }else if (strcmp((char*)buf_state,"LOADING") == 0){ 
+        current_state = LOADING;
+      }else if (strcmp((char*)buf_state,"PLAN_PATH_TO_BASE") == 0){ 
+        current_state = PLAN_PATH_TO_BASE;
+      }else if (strcmp((char*)buf_state,"PLAN_PATH_TO_TABLE") == 0){ 
+        current_state = PLAN_PATH_TO_TABLE;
+      }else { 
+        current_state = UNDEFINED;
+      }
+    } 
 }
 
 int main(void) {
@@ -128,9 +170,7 @@ int main(void) {
   NRF_LOG_DEFAULT_BACKENDS_INIT();
   printf("Initialized RTT!\n");
 
-  //kobukiSensorPoll(&initial_sensors);
-  KobukiState_t state = DRIVING;
-  
+  //kobukiSensorPoll(&initial_sensors);  
 
   // initialize display
   nrf_drv_spi_t spi_instance = NRF_DRV_SPI_INSTANCE(1);
@@ -159,23 +199,27 @@ int main(void) {
   simple_ble_app = simple_ble_init(&ble_config);
 
   simple_ble_add_service(&generic_service);
+  simple_ble_add_service(&state_service);
 
   simple_ble_add_characteristic(1, 1, 0, 0,
     sizeof(uint8_t),&button_press,
     &generic_service, &get_button_press);
   
-  /*simple_ble_add_characteristic(1, 1, 0, 0,
+  simple_ble_add_characteristic(1, 1, 0, 0,
       sizeof(uint8_t)*6, error_data,
-      &generic_service, &test_error_data); *///send 6 bytes, 2 bytes for each error 
+      &generic_service, &test_error_data); ///send 6 bytes, 2 bytes for each error 
 
   simple_ble_add_characteristic(1, 1, 0, 0,
-      sizeof(led_state), (uint8_t*)&led_state,
-      &generic_service, &led1_state_char);
-
-  simple_ble_add_characteristic(1, 1, 0, 0,
-      sizeof(char)*16,buf_disp,
+      sizeof(char)*64, buf_disp,
       &generic_service, &display_string_data);
 
+  simple_ble_add_characteristic(1, 1, 0, 0,
+      sizeof(char)*16, buf_state,
+      &state_service, &get_kobubki_state);
+  
+  simple_ble_add_characteristic(1, 1, 0, 0,
+      sizeof(led_state), (uint8_t*)&led_state,
+      &state_service, &led1_state_char);
 
   // initialize i2c master (two wire interface)
   /*nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
@@ -192,17 +236,27 @@ int main(void) {
   simple_ble_adv_only_name();
   
   while(1) {
-    
-    kobukiSensorPoll(&sensors);
-    /*if (is_button_pressed(&sensors)) {
-      g_button_pressed = 1;
-    }*/
-    int check_button = is_button_pressed(&sensors);
-    if (check_button){ 
-      g_button_pressed = 1;
+    switch (current_state){ 
+      case LOADING:
+      case UNLOADING: {
+        kobukiSensorPoll(&sensors);
+        int check_button = is_button_pressed(&sensors);
+        if (check_button) { 
+          g_button_pressed = 1;
+        }
+        break;
+      } 
+      case DELIVERING_ORDER: 
+      case RETURNING: 
+        drive();
+        break;
+      case PLAN_PATH_TO_BASE:
+      case PLAN_PATH_TO_TABLE: 
+      case IDLE: 
+      default: 
+        //nrf_delay_ms(250);somehow this f**ks up everthing
+        break; 
     }
-    drive();
-    //nrf_delay_ms(250);
   }
 }
 
